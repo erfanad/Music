e1b7d768ec173644918b2b518f6d89d597d462dc
erfanad <erfana@go.stockton.edu>
2016-10-18 22:39:57 -0400

    edit albums

--- src/MusicFall2016/Controllers/AlbumsController.cs
+++ src/MusicFall2016/Controllers/AlbumsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using MusicFall2016.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,12 +25,44 @@ namespace MusicFall2016.Controllers
         }
         public IActionResult Details(int? id)
         {
-            Album album = db.Albums.Where(a => a.AlbumID == id);
+
+            var albums = db.Albums.Include(a => a.Artist).Include(a => a.Genre).ToList();
+            Album album = albums.Single(a => a.AlbumID == id);
             if (album == null)
             {
                 return HttpNotFound();
             }
             return View(album);
+        }
+        public IActionResult Edit(int? id)
+        {
+            var albums = db.Albums.Include(a => a.Artist).Include(a => a.Genre).ToList();
+            Album album = albums.Single(a => a.AlbumID == id);
+            ViewBag.Artists = new SelectList(db.Artists.ToList(), "ArtistID", "Name");
+            ViewBag.Genres = new SelectList(db.Genres.ToList(), "GenreID", "Name");
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+        [HttpPost]
+        public IActionResult Edit(Album album, int? id)
+        {
+            var albums = db.Albums.Include(a => a.Artist).Include(a => a.Genre).ToList();
+            album = albums.Single(a => a.AlbumID == id);
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(album);
+        }
+
+        private IActionResult HttpNotFound()
+        { 
+            throw new NotImplementedException();
         }
     }
 }
